2759efaed5fb7478dc8d8b14590adfe495052d44
Noriaki Watanabe <nabeyang@gmail.com>
2017-09-02 23:31:10 +0900

    add test test_exist_node

--- provision.sh
+++ provision.sh
@@ -4,7 +4,4 @@ sudo apt-get install -y git
 cd /vagrant
 bash -xve ./test/travis_ros_install.bash
 source ~/catkin_ws/devel/setup.bash
-bash -xve ./test/travis_package_make.bash
-source ~/catkin_ws/devel/setup.bash
-bash -xve ./test/travis_prepare_dummy_files.bash
-rostest pimouse_ros test.launch
+./run_test.bash
--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -2,7 +2,22 @@
 #encoding: utf-8
 import unittest, rostest
 import rosnode, rospy
+import time
 from pimouse_ros.msg import LightSensorValues
 
 class LightsensorTest(unittest.TestCase):
-  pass
+  def setUp(self):
+    self.count = 0
+    rospy.Subscriber('/lightsensors', LightSensorValues, self.callback)
+    self.values = LightSensorValues()
+  def callback(self, data):
+    self.count += 1
+    self.values = data
+  def test_node_exist(self):
+    nodes = rosnode.get_node_names()
+    self.assertIn('/lightsensors', nodes, "node does not exist")
+
+if __name__ == '__main__':
+  time.sleep(3)
+  rospy.init_node('travis_test_lightsensors')
+  rostest.rosrun('pimouse_ros', 'travis_test_lightsensors', LightsensorTest)
