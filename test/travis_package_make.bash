3f7443160874f73950358d660d0408a097a0643f
Noriaki Watanabe <nabeyang@gmail.com>
2017-09-02 17:13:30 +0900

    .vagrantをrsyncしないように変更

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -1,4 +1,4 @@
 #!/bin/bash -xve
-rsync -av --exclude='.git/' ./ ~/catkin_ws/src/pimouse_ros/
+rsync -av --exclude='.git/' --exclude='.vagrant' ./ ~/catkin_ws/src/pimouse_ros/
 cd ~/catkin_ws
 catkin_make
