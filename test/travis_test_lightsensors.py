8c7aec99ed3b3780ca319b5b71361e0aa34c877f
Noriaki Watanabe <nabeyang@gmail.com>
2017-09-02 21:33:44 +0900

    add test case

---
+++ test/travis_test_lightsensors.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#encoding: utf-8
+import unittest, rostest
+import rosnode, rospy
+from pimouse_ros.msg import LightSensorValues
+
+class LightsensorTest(unittest.TestCase):
+  pass
