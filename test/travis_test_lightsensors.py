08e9816c0824122a670225859aa05bad20a75736
Noriaki Watanabe <nabeyang@gmail.com>
2017-09-03 16:56:43 +0900

    add test for change rospy.Rate

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -36,6 +36,14 @@ class LightsensorTest(unittest.TestCase):
     self.assertFalse(self.count == 0, "cannot subscribe the topic")
     self.check_values(4321, 123, 0, -1)
 
+  def test_change_parameter(self):
+    rospy.set_param('lightsensors_freq', 1)
+    time.sleep(2)
+    c_prev = self.count
+    time.sleep(3)
+    self.assertTrue(self.count < c_prev + 4, "freq does not change")
+    self.assertFalse(self.count == c_prev, "subscriber is stopped")
+
 if __name__ == '__main__':
   time.sleep(3)
   rospy.init_node('travis_test_lightsensors')
